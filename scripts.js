c545182654a9746a999d837b139c68bc5002e156
kareeemashrafm <kareeemashrafm@gmail.com>
2025-08-31 14:15:49 +0300

    final

--- scripts.js
+++ scripts.js
@@ -1,17 +1,54 @@
 const books= [];
 
 
-function book(author,title,pages,read){
+function Book(title,author,pages){
   this.id=crypto.randomUUID();
   this.author=author;
   this.title=title;
   this.pages=pages;
-  this.read=read;
 
 }
-function addbook(){
+function addbook(title,author,pages){
 
-  const book=new book(author,title,pages,read);
+  const book=new Book(title,author,pages);
   books.push(book);
+  displaybook();
 };
+
+function displaybook(){
+  const container=document.getElementById("bookcontainer");
+  container.innerHTML="";
+
+  books.forEach((book) =>{
+
+    const card=document.createElement("div");
+    card.classList.add("bookcard");
+    card.innerHTML=`
+      <p>Title: ${book.title}</p>
+      <p>Author: ${book.author}</p>
+      <p>Pages: ${book.pages}</p>
+    `
+    container.appendChild(card);
+  })
+  
+  
+}
+
+const addbutton=document.getElementById("addbutton");
+  const form=document.getElementById("form");
+  addbutton.addEventListener("click",()=>{
+    form.style.display="block";
+  })
+
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+
+  addbook(title,author,pages);
+  this.reset();
+  this.style.display="none";
+});
+
 
